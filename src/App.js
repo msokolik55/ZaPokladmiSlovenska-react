6ff199a8602de22eebf15561de499c8441f4684b
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2021-03-16 22:50:14 +0100

    Added basic functions
    - sidebar
    - changing title, YT video, description

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedID, setSelectedID] = useState(0);
+    const [title, setTitle] = useState("");
+    const [ytLink, setYtLink] = useState("");
+    const [description, setDescription] = useState("");
+    
+    const titles = [
+        {
+            id: 0,
+            value: "1st title",
+            ytLink: "https://www.youtube.com/embed/I_Xv360Ihl0",
+            description: "Some data",
+        },
+        {
+            id: 1,
+            value: "2nd title",
+            ytLink: "https://www.youtube.com/embed/m8R6LM4evDw",
+            description: "Some data about 2nd",
+        },
+        {
+            id: 2,
+            value: "3rd title",
+            ytLink: "https://www.youtube.com/embed/fyX82Nqg56I",
+            description: "Some data from somewhere",
+        },
+    ]
+
+    useEffect(() => {
+        let selected = titles.filter((obj) => obj.id === selectedID)[0];
+        setTitle(selected.value);
+        setYtLink(selected.ytLink);
+        setDescription(selected.description);
+        // eslint-disable-next-line
+    }, [selectedID]);
+
+    const changeTitle = (idTarget) => {
+        setSelectedID(idTarget);
+    }
+
+    return (
+        <div
+            //style={{
+            //    width: "100%",
+            //    display: "grid",
+            //    gridTemplateColumns: "repeat(2, 0fr)",
+            //    gridGap: 20,
+            //}}
+            className={"Row"}
+            >
+            <div className={"Drawer Column"}>
+                {
+                    titles.map((obj) => (
+                        <p
+                            className={"Drawer-item"}
+                            key={obj.id}
+                            onClick={() => changeTitle(obj.id)}
+                            >
+                            {obj.id + 1}. option
+                        </p>
+                    ))
+                }
+            </div>
+            <div className={"Content Column"}>
+                <h1>{title}</h1>
+                <iframe
+                    title={title}
+                    width="100%"
+                    height="315"
+                    src={ytLink}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    >
+                </iframe>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
